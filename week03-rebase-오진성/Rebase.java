752877215b0a39b7b3f5b1af2c83457ab113edfc
OHJINSEONG <ojseong0828@naver.com>
2022-09-04 21:58:04 +0900

    2차 커밋2차 커밋

--- week03-rebase-오진성/Rebase.java
+++ week03-rebase-오진성/Rebase.java
@@ -1,5 +1,11 @@
 public class Rebase {
     public static void main(String[] args) {
+        Rebase application = new Rebase();
+
+        application.run();
+    }
+
+    private void run() {
         
     }
 }
